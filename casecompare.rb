2610ff0c8b36c75b463e42f105d5d737272cad5b
Andrey Savich <46422808+pieckenst@users.noreply.github.com>
2021-05-12 15:03:45 +0300

    Create casecompare.rb

---
+++ casecompare.rb
@@ -0,0 +1,19 @@
+case weight
+when 1,2,3
+ puts "Low"
+when 4,5,6
+ puts "High"
+#when 10,11,12,13
+ #puts "Obese"
+else
+ puts "Invalid"
+endcase weight
+when 1,2,3
+ puts "Low"
+when 4,5,6
+ puts "High"
+#when 10,11,12,13
+ #puts "Obese"
+else
+ puts "Invalid"
+end
